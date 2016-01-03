ee36d6cb002af6b67cffbfc55841cc7df7e66ff7
Henrik Sjööh <henriiik@gmail.com>
2016-01-03 21:30:49 +0100

    add rustfmt

--- src/extension.ts
+++ src/extension.ts
@@ -2,14 +2,11 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
 import {build} from "./cargo";
+import {RustDocumentFormattingEditProvider} from "./rustfmt";
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-
-	context.subscriptions.push(vscode.commands.registerCommand("rust.build", build));
+    let RUST: vscode.DocumentFilter = { language: "rust", scheme: "file" };
+    context.subscriptions.push(vscode.languages.registerDocumentFormattingEditProvider(RUST, new RustDocumentFormattingEditProvider()));
+    context.subscriptions.push(vscode.commands.registerCommand("rust.build", build));
 }
---
+++ src/rustfmt.ts
@@ -0,0 +1,60 @@
+import * as vscode from "vscode";
+import * as cp from "child_process";
+
+let errorRegex = /(\w+):(\d+):(\d+):\s(\d+):(\d+)\s(\w+):\s(.*)/g;
+
+export class RustDocumentFormattingEditProvider implements vscode.DocumentFormattingEditProvider {
+    provideDocumentFormattingEdits(document: vscode.TextDocument, options: vscode.FormattingOptions, token: vscode.CancellationToken): Thenable<vscode.TextEdit[]> {
+        return new Promise<vscode.TextEdit[]>((resolve, reject) => {
+            let collection = vscode.languages.createDiagnosticCollection("rustfmt")
+            collection.clear();
+
+            let child = cp.spawn("rustfmt", []);
+            child.stdin.write(document.getText());
+            child.stdin.end();
+
+            let formatted = "";
+            child.stdout.on("data", (data: Buffer) => {
+                formatted += data.toString();
+            });
+
+            let diagnostics: vscode.Diagnostic[] = [];
+            child.stderr.on("data", (data: Buffer) => {
+                let output = data.toString();
+                let message: RegExpExecArray;
+                while (message = errorRegex.exec(output)) {
+                    diagnostics.push(formatMessage(message));
+                }
+            });
+
+            child.on("close", (code) => {
+                if (code > 0) {
+                    collection.set(document.uri, diagnostics);
+                    vscode.window.showErrorMessage("Could not format file.");
+                    reject(null);
+                } else {
+                    let range = new vscode.Range(
+                        0,
+                        0,
+                        document.lineCount - 1,
+                        Number.MAX_VALUE
+                    );
+                    resolve([new vscode.TextEdit(range, formatted)]);
+                }
+            });
+        });
+    }
+}
+
+function formatMessage(message: RegExpExecArray): vscode.Diagnostic {
+    return new vscode.Diagnostic(
+        new vscode.Range(
+            Number(message[2]) - 1,
+            Number(message[3]),
+            Number(message[4]) - 1,
+            Number(message[5])
+        ),
+        message[7],
+        vscode.DiagnosticSeverity.Error
+    );
+}
