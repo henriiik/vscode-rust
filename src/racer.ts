7919d25c17069d5bffa30f35d2e85bbd572d631c
Henrik Sjööh <henrik.sjooh@configura.com>
2016-01-04 12:13:39 +0100

    extract specifics

--- src/racer.ts
+++ src/racer.ts
@@ -6,30 +6,40 @@ let itemKindMap = {
     "Module": vscode.CompletionItemKind.Module,
 };
 
+function spawnRacer(document: vscode.TextDocument, position: vscode.Position, command: string): Thenable<cp.ChildProcess> {
+    return document.save().then(() => {
+        return cp.spawn("racer", [
+            "--interface",
+            "tab-text",
+            command,
+            String(position.line + 1),
+            String(position.character),
+            document.fileName
+        ]);
+    });
+}
+
+function makeCompletionItem(data: string[], root: string): vscode.CompletionItem {
+    let item = new vscode.CompletionItem(data[1]);
+    item.kind = itemKindMap[data[6]];
+    item.detail = data[5].replace(root, "");
+    item.documentation = data[7];
+    return item;
+}
+
 export class RustCompletionItemProvider implements vscode.CompletionItemProvider {
     provideCompletionItems(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken): Thenable<vscode.CompletionItem[]> {
-        return document.save().then(() => {
+        return spawnRacer(document, position, "complete-with-snippet").then((child) => {
             return new Promise((resolve, reject) => {
                 let root = vscode.workspace.rootPath + "/";
                 let items = [];
 
-                let child = cp.spawn("racer", ["--interface", "tab-text", "complete-with-snippet", String(position.line + 1), String(position.character), document.fileName]);
-                child.stdin.write(document.getText());
-                child.stdin.end();
-
                 child.stdout.on("data", (data: Buffer) => {
                     let lines = data.toString().split("\n");
-                    let prefix = "";
-                    for (var line of lines) {
+                    for (let line of lines) {
                         let data = line.split("\t");
-                        if (data[0] === "PREFIX") {
-                            prefix = data[3];
-                        } else if (data[0] === "MATCH") {
-                            let item = new vscode.CompletionItem(data[1]);
-                            item.kind = itemKindMap[data[6]];
-                            item.detail = data[5].replace(root, "");
-                            item.documentation = data[7];
-                            items.push(item);
+                        if (data[0] === "MATCH") {
+                            items.push(makeCompletionItem(data, root));
                         }
                     }
                 });
@@ -52,30 +62,27 @@ export class RustCompletionItemProvider implements vscode.CompletionItemProvider
     }
 }
 
+function makeDefinition(data: string[]): vscode.Definition {
+    return new vscode.Location(vscode.Uri.file(data[4]), new vscode.Position(
+        Number(data[2]) - 1,
+        Number(data[3])
+    ));
+}
+
 
 export class RustDefinitionProvider implements vscode.DefinitionProvider {
     provideDefinition(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken): Thenable<vscode.Definition> {
-        return document.save().then(() => {
+        return spawnRacer(document, position, "find-definition").then((child) => {
             return new Promise((resolve, reject) => {
                 let root = vscode.workspace.rootPath + "/";
                 let def: vscode.Definition = null;
 
-                let child = cp.spawn("racer", ["--interface", "tab-text", "find-definition", String(position.line + 1), String(position.character), document.fileName]);
-                child.stdin.write(document.getText());
-                child.stdin.end();
-
                 child.stdout.on("data", (data: Buffer) => {
                     let lines = data.toString().split("\n");
-                    let prefix = "";
-                    for (var line of lines) {
+                    for (let line of lines) {
                         let data = line.split("\t");
-                        if (data[0] === "PREFIX") {
-                            prefix = data[3];
-                        } else if (data[0] === "MATCH") {
-                            def = new vscode.Location(vscode.Uri.file(data[4]), new vscode.Position(
-                                Number(data[2]) - 1,
-                                Number(data[3])
-                            ));
+                        if (data[0] === "MATCH") {
+                            def = makeDefinition(data);
                         }
                     }
                 });
@@ -92,7 +99,6 @@ export class RustDefinitionProvider implements vscode.DefinitionProvider {
                         resolve(def);
                     }
                 });
-
             });
         });
     }
