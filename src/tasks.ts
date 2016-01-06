e5b559c17423d8f118ba222fc07fc1c722b38e84
Henrik Sjööh <henriiik@gmail.com>
2016-01-06 16:06:39 +0100

    add fmt task

--- src/tasks.ts
+++ src/tasks.ts
@@ -52,6 +52,10 @@ let tasks = {
         {
             "taskName": "doc",
             "problemMatcher": problemMatcher
+        },
+        {
+            "taskName": "fmt",
+            "problemMatcher": problemMatcher
         }
     ]
 };
